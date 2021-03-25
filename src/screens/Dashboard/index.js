8705af2c46d86d881d5256df85c2d4cf4a4271de
Júlio César <julio.souzam@gmail.com>
2021-03-24 22:54:25 -0300

    change url login

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -3,7 +3,9 @@ export function Dashboard() {
     <>
       <h1>Hello, Boss</h1>
 
-      <a href="https://github.com/login/oauth/authorize?client_id=48048c83679fbe59a9c2&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin%2Fcallback&scope=user">
+      <a
+        href={`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&redirect_uri=https%3A%2F%2Flogin-github.netlify.app%2Flogin%2Fcallback&scope=user`}
+      >
         Entrar
       </a>
     </>
